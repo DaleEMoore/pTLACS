fa991b646c59fe110e8bd31782d545648a0f9302
Dale E. Moore <DaleEMoore@gMail.Com>
2017-02-23 12:55:22 -0600

    Turtle position and screen window management is non-obvious to Dale.

--- exercise4.py
+++ exercise4.py
@@ -1,22 +1,32 @@
 #/bin/python3
 
+# TODO; there are more exercises in section 4.3 page 31.
+# TODO; A 2 monitor system gets the previous centered on the 2 monitor break; move it.
+
 import polygon
+import turtle
 
 print("Exercise 4.1")
 # download http://greenteapress.com/thinkpython2/code/polygon.py
+# http://web.cecs.pdx.edu/~lmd/cs161/turtle-excerpt.htm (not everything works with Dale's version of turtle.
+# https://docs.python.org/3/library/turtle.html#module-turtle
 
-bob = 3
-radius = 2
-# TODO; I don't have this setup correctly. Perhaps I should re-read chapter 4. I'm getting the error:
-"""
-Traceback (most recent call last):
-  File "/home/dalem/PycharmProjects/pTLACS/exercise4.py", line 12, in <module>
-    polygon.circle(bob, radius)
-  File "/home/dalem/PycharmProjects/pTLACS/polygon.py", line 79, in circle
-    arc(t, r, 360)
-  File "/home/dalem/PycharmProjects/pTLACS/polygon.py", line 68, in arc
-    t.lt(step_angle/2)
-AttributeError: 'int' object has no attribute 'lt'
-"""
+bob = turtle.Turtle()
+
+#bob.reset()
+#bob.screen.setworldcoordinates(100,100,250,207.5)
+#bob.screen.setworldcoordinates(-50,-7.5,50,7.5)
+#for _ in range(72):
+#     left(10)
+#for _ in range(8):
+#     left(45); fd(2)   # a regular octagon
+#reply = input("Tap ENTER to continue...")
+
+#bob.reset()
+bob.penup()
+#bob.setx(100)
+#bob.sety(100)
+radius = 200
+bob.pendown()
 polygon.circle(bob, radius)
-
+reply = input("Tap ENTER to continue...")
