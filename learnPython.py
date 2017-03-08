7d85c49a3de760d0b642392869756510c5daaf18
Dale E. Moore <DaleEMoore@gMail.Com>
2017-03-07 21:17:41 -0600

    Adding Learn Python appliation for Android stuff.

---
+++ learnPython.py
@@ -0,0 +1,20 @@
+# learnPython.py
+# From Learn Python application on Android.
+
+print("Exponentation")
+x2 = 2**5
+print(x2)
+x2 = 9 ** (1/2)
+print(x2)
+x2 = (1 + 3) ** 2
+print(x2)
+
+print("Quotient & Remainder")
+x2 = 20 // 6        # Floor division
+print(x2)
+x2 = 1.25 % 0.5     # Modulo
+print(x2)
+x2 = ( 5 // 2)
+print(x2)
+x2 = 7 % ( 5 // 2)
+print(x2)
